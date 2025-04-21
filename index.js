2cc1259d97a68ff7aa29dacb6debf12dc6265d9f
Vladimir <mezenkovvladimir06@gmail.com>
2025-04-21 23:26:27 +0300

    Начало реализации пользовательского интерфейса / багфикс

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const express = require('express');
-const {Sequelize} = require("sequelize");
 const {sequelize} = require("./db");
-const path = require("path");
-
 const app = express();
 require('dotenv').config();
 
@@ -10,13 +7,15 @@ const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '127.0.0.1';
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src', 'views'));
-console.log(app.get('views'))
+app.set('views', './src/views');
+app.use(express.static('./public'));
+
 
 app.use(express.json());
 app.use('/api/branches', require('./src/routes/branches'));
 app.use('/api/employees', require('./src/routes/employees'));
 app.use('/api/positions', require('./src/routes/positions'));
+app.use('/', require('./src/routes/index'));
 
 const server = app.listen(PORT, HOST, () => {
     try {
